1f81fb32b9c1a9904b08069674c7070373e23738
meipey <82648623+meipey@users.noreply.github.com>
2021-09-14 17:00:07 +0200

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-new_home","displayName":"new_home","link":{"linkType":"LinkTypePage","href":"#!page-new_home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-new_home","displayName":"new_home","link":{"linkType":"LinkTypePage","href":"#!page-new_home"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-di2","displayName":"Di2","link":{"linkType":"LinkTypePage","href":"#!page-di2"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true}]}
